76b9ae1ad54944c148f6224decbc778d587f5565
桃源道不清 <xubiaoyu040827@outlook.com>
2024-11-15 16:37:17 +0800

    创建user数据表

---
+++ src/main/resources/sql/user.sql
@@ -0,0 +1,7 @@
+create table if not exists user (
+    userName varchar(20) not null,
+    password varcahr(20) not null,
+    priority varchar(20) not null,
+    userId varchar(20) not null,
+    primary key (userId)
+)
