ad7b6b0fa6355f97fa3db2113571c13cb160c011
dudu233 <1147793932@qq.com>
2024-11-15 09:31:29 +0800

    添加页面进行测试

--- src/main/java/top/mcso/sms/controller/MainController.java
+++ src/main/java/top/mcso/sms/controller/MainController.java
@@ -1,12 +1,13 @@
 package top.mcso.sms.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class MainController {
     @RequestMapping("/")
-    public String home() {
-        return "Hello World";
+    public String home(Model model) {
+        return "login";
     }
 }
