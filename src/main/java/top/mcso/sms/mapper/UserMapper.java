553169e1cec41d46e2c163bead4ee39c97194974
桃源道不清 <xubiaoyu040827@outlook.com>
2024-11-16 19:46:33 +0800

    编写StudentMapper

--- src/main/java/top/mcso/sms/mapper/UserMapper.java
+++ src/main/java/top/mcso/sms/mapper/UserMapper.java
@@ -1,16 +1,37 @@
 package top.mcso.sms.mapper;
 
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.*;
 import top.mcso.sms.entity.User;
+
+import java.util.List;
 
 
 @Mapper
 public interface UserMapper {
-    //添加新用户
-    @Insert("insert into user(username, password, priority, user_number) values (#{username},#{password},#{priority},#{user_number})")
-    User addUser(@Param("newUser") User newUser);
+    // 插入用户
+    @Insert("insert into user (username, password, priority, user_number) values (#{username}, #{password}, #{priority}, #{userNumber})")
+    @Options(useGeneratedKeys = true, keyProperty = "userNumber")
+    int insert(User user);
+
+    // 删除用户
+    @Delete("delete from user where user_number = #{userNumber}")
+    int deleteByUserNumber(@Param("userNumber") String userNumber);
+
+    // 更新用户
+    @Update("update user set username = #{username}, password = #{password}, priority = #{priority} where user_number = #{userNumber}")
+    int update(User user);
+
+    // 根据用户编号查询用户
+    @Select("select * from user where user_number = #{userNumber}")
+    User findByUserNumber(@Param("userNumber") String userNumber);
+
+    // 查询所有用户
+    @Select("select * from user")
+    List<User> findAll();
+
+    // 根据用户名查询用户
+    @Select("select * from user where username = #{username}")
+    User findByUsername(@Param("username") String username);
 
 }
 
