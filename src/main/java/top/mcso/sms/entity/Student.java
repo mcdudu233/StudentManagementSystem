776cb5099705f4b0e66015280d1d6446c24cdf90
yzc23678 <2367853358@qq.com>
2024-11-16 20:05:59 +0800

    更新了student

--- src/main/java/top/mcso/sms/entity/Student.java
+++ src/main/java/top/mcso/sms/entity/Student.java
@@ -14,10 +14,12 @@ public class Student {
     private String gender;
     private String Class;
     private int age;
+    private String address;
+    private String telephone;
 
 
     @Override
     public String toString() {
-        return "学生 [学号=" + studentNumber  + ", 姓名=" + name + ", 性别=" + gender + ", 年龄=" + age + ", 班级= " + Class + "]";
+        return "学生 [学号=" + studentNumber  + ", 姓名=" + name + ", 性别=" + gender + ", 年龄=" + age + ", 班级= " + Class + ", 地址=" + address + "， 电话=" + telephone + "]";
     }
 }
