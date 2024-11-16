db32084718b5196ef51201ac0d47954f4d07b689
桃源道不清 <xubiaoyu040827@outlook.com>
2024-11-16 19:14:52 +0800

    更新数据库

--- src/main/resources/sql/grade.sql
+++ src/main/resources/sql/grade.sql
@@ -1,6 +1,7 @@
-create table grade(
-    courseNumber varchar(20) not null ,
-    studentNumber varchar(20) not null,
-    grade float not null,
-    primary key (studentNumber)
+create table grade
+(
+    course_number  varchar(20) not null,
+    student_number varchar(20) not null,
+    grade          float       not null,
+    primary key (student_number)
 )
