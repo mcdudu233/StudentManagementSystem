4c8309c779bcd3520446c69b800d50049c7f1530
dudu233 <1147793932@qq.com>
2024-11-17 13:12:28 +0800

    连接到数据库

--- src/main/java/top/mcso/sms/StudentManagementSystemApplication.java
+++ src/main/java/top/mcso/sms/StudentManagementSystemApplication.java
@@ -2,12 +2,15 @@ package top.mcso.sms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-// @SpringBootApplication
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class}) // 这里先不用数据库
+/**
+ * 程序入口
+ *
+ * @author dudu233
+ */
+
+@SpringBootApplication
 public class StudentManagementSystemApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(StudentManagementSystemApplication.class, args);
     }
