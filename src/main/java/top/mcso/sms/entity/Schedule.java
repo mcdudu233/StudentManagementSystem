638b8d4b37e96b0abf8898ba2811463f283404cf
桃源道不清 <xubiaoyu040827@outlook.com>
2024-11-17 10:56:17 +0800

    增加选课表数据表和类

---
+++ src/main/java/top/mcso/sms/entity/Schedule.java
@@ -0,0 +1,20 @@
+package top.mcso.sms.entity;
+
+import lombok.*;
+
+@Setter
+@Getter
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Schedule {
+    private String studentNumber;
+    private String studentName;
+    private String courseName;
+    private String teacherName;
+
+    @Override
+    public String toString() {
+        return "Schedule [studentNumber=" + studentNumber + ", studentName=" + studentName + ", courseName=" + courseName + ", teacherName=" + teacherName + "]";
+    }
+}
