4e2e6a4f8fd6cd662bd9192461387f6a2c7e9c64
桃源道不清 <xubiaoyu040827@outlook.com>
2024-11-17 10:33:34 +0800

    增加CourseMapper

---
+++ src/main/java/top/mcso/sms/mapper/CourseMapper.java
@@ -0,0 +1,36 @@
+package top.mcso.sms.mapper;
+
+import org.apache.ibatis.annotations.*;
+import top.mcso.sms.entity.Course;
+
+import java.util.List;
+
+@Mapper
+public interface CourseMapper {
+    // 插入课程信息
+    @Insert("insert into course (course_number, course_name, priority_course) " +
+            "values (#{courseNumber}, #{courseName}, #{priorityCourse})")
+    int insertCourse(Course course);
+
+    // 根据课程编号删除课程
+    @Delete("delete from course where course_number = #{courseNumber}")
+    int deleteCourseByCourseNumber(String courseNumber);
+
+    // 更新课程信息
+    @Update("update course set course_name = #{courseName}, priority_course = #{priorityCourse} " +
+            "where course_number = #{courseNumber}")
+    int updateCourse(Course course);
+
+    // 根据课程编号查询课程
+    @Select("select * from course where course_number = #{courseNumber}")
+    Course selectCourseByCourseNumber(String courseNumber);
+
+    // 查询所有课程
+    @Select("select * from course")
+    List<Course> selectAllCourses();
+
+    // 根据优先级课程查询课程列表
+    @Select("select * from course where priority_course = #{priorityCourse}")
+    List<Course> selectCoursesByPriorityCourse(String priorityCourse);
+
+}
