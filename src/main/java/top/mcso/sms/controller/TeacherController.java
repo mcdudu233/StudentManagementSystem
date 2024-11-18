92dc0f510f0649d6db4417bfa1a12208b8b6dff3
qzn <317721272@qq.com>
2024-11-18 23:55:27 +0800

    解决进入index页面后，url任然为login的问题

--- src/main/java/top/mcso/sms/controller/TeacherController.java
+++ src/main/java/top/mcso/sms/controller/TeacherController.java
@@ -2,12 +2,13 @@ package top.mcso.sms.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
 
 @Controller
 public class TeacherController {
 
     @GetMapping("/my-courses")
-    public String myCourses() {
+    public String myCourses(Model model) {
         return "teacher/class";
     }
 
