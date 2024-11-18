85d5b5a23c28e8f311ba4fc813edb48af8b886fe
qzn <317721272@qq.com>
2024-11-18 23:23:05 +0800

    添加teacher的前端页面文件，内容无，添加用来跳转的teacherController

---
+++ src/main/java/top/mcso/sms/controller/TeacherController.java
@@ -0,0 +1,23 @@
+package top.mcso.sms.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TeacherController {
+
+    @GetMapping("/my-courses")
+    public String myCourses() {
+        return "teacher/class";
+    }
+
+    @GetMapping("/student-management")
+    public String studentManagement() {
+        return "teacher/student";
+    }
+
+    @GetMapping("/grade-management")
+    public String gradeManagement() {
+        return "teacher/grade";
+    }
+}
