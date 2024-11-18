f0fc71380bb1a66ab9ea8e8705a256c021d7dbb3
dudu233 <1147793932@qq.com>
2024-11-18 22:51:00 +0800

    更新主页显示用户信息

--- src/main/java/top/mcso/sms/controller/MainController.java
+++ src/main/java/top/mcso/sms/controller/MainController.java
@@ -1,5 +1,9 @@
 package top.mcso.sms.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,22 +12,47 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * 主页控制器
+ * 处理主页的内容
+ *
+ * @author dudu233
+ * @create: 2024-11-15 16:24
+ * @version: 1.0
+ */
+
 @Controller
 public class MainController {
 
     // 主页面
     @RequestMapping("/")
     public String home(Model model) {
-        String role = "ROLE_TEACHER";
-        // 根据角色加载不同的数据
-        model.addAttribute("role", role);
-        if ("ROLE_ADMIN".equals(role)) {
-            model.addAttribute("dashboardData", getAdminDashboardData());
-        } else if ("ROLE_TEACHER".equals(role)) {
-            model.addAttribute("dashboardData", getTeacherDashboardData());
-        } else if ("ROLE_STUDENT".equals(role)) {
-            model.addAttribute("dashboardData", getStudentDashboardData());
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        // 未登录则跳转到登录页面
+        if (authentication == null) {
+            return "redirect:/login";
         }
+
+        // 获取用户信息
+        User principal = (User) authentication.getPrincipal();
+        String role = "ROLE_null";
+        for (GrantedAuthority ga : principal.getAuthorities()) {
+            role = ga.getAuthority();
+        }
+
+        model.addAttribute("role", role);
+        model.addAttribute("user", principal.getUsername());
+        // 根据角色加载不同的数据
+        if ("ROLE_admin".equals(role)) {
+            model.addAttribute("dashboardData", getAdminDashboardData());
+        } else if ("ROLE_teacher".equals(role)) {
+            model.addAttribute("dashboardData", getTeacherDashboardData());
+        } else if ("ROLE_student".equals(role)) {
+            model.addAttribute("dashboardData", getStudentDashboardData());
+        } else {
+            model.addAttribute("dashboardData", getAdminDashboardData());
+        }
+
         return "index";
     }
 
