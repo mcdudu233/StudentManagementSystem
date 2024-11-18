98e25e172f6d2a4007679a2bea1a9ed458e15638
dudu233 <1147793932@qq.com>
2024-11-18 21:36:03 +0800

    解决lombok报错

--- src/main/java/top/mcso/sms/entity/Student.java
+++ src/main/java/top/mcso/sms/entity/Student.java
@@ -1,15 +1,17 @@
 package top.mcso.sms.entity;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Student {
-    private String studentNumber ;
+    private String studentNumber;
     private String name;
     private String gender;
-    private String Class;
+    private String classes;
     private int age;
     private String address;
     private String telephone;
@@ -17,6 +19,6 @@ public class Student {
 
     @Override
     public String toString() {
-        return "学生 [学号=" + studentNumber  + ", 姓名=" + name + ", 性别=" + gender + ", 年龄=" + age + ", 班级= " + Class + ", 地址=" + address + "， 电话=" + telephone + "]";
+        return "学生 [学号=" + studentNumber + ", 姓名=" + name + ", 性别=" + gender + ", 年龄=" + age + ", 班级= " + classes + ", 地址=" + address + "， 电话=" + telephone + "]";
     }
 }
