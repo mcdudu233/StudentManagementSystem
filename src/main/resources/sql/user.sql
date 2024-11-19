d4d7c13758237cdace45bc600d525975922b7566
桃源道不清 <xubiaoyu040827@outlook.com>
2024-11-19 20:28:45 +0800

    删除外键，更改相应类型

--- src/main/resources/sql/user.sql
+++ src/main/resources/sql/user.sql
@@ -3,6 +3,5 @@ create table user
     user_number varchar(20) not null,
     password    varchar(20) not null,
     priority    varchar(20) not null,
-    primary key (user_number),
-
+    primary key (user_number)
 )
