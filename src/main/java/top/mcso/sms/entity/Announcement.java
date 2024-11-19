264c44e662ac75a961f06ad782f3e0b794154364
yzc23678 <2367853358@qq.com>
2024-11-19 13:19:15 +0800

    更新了所有类

--- src/main/java/top/mcso/sms/entity/Announcement.java
+++ src/main/java/top/mcso/sms/entity/Announcement.java
@@ -2,8 +2,6 @@ package top.mcso.sms.entity;
 
 import lombok.*;
 
-@Setter
-@Getter
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -12,9 +10,4 @@ public class Announcement {
     private String id ;
     private String announcement;
 
-
-    @Override
-    public String toString() {
-        return "公告 [序号=" + id + ", 公告=" + announcement + "]";
-    }
 }
