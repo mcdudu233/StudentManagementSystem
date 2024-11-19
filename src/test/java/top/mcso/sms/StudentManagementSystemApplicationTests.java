0a1630a0f3801c7c47bda847dc952bf705716619
dudu233 <1147793932@qq.com>
2024-11-19 17:00:42 +0800

    修改测试类，测试数据库

--- src/test/java/top/mcso/sms/StudentManagementSystemApplicationTests.java
+++ src/test/java/top/mcso/sms/StudentManagementSystemApplicationTests.java
@@ -4,53 +4,42 @@ import jakarta.annotation.Resource;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import top.mcso.sms.entity.User;
+import top.mcso.sms.service.StudentService;
 import top.mcso.sms.service.TeacherService;
 import top.mcso.sms.service.UserService;
+
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class StudentManagementSystemApplicationTests {
-
+    // 导入要测试的服务
     @Resource
     private UserService userService;
-    //测试Teacher
+    @Resource
+    private StudentService studentService;
     @Resource
     private TeacherService teacherService;
 
-    //测试User
     @Test
-    void testInsertUser() {
-        User user = new User("1", "2", "3");
+    void testDatabase() {
+
+        // 随机产生用户名和密码
+        String username = UUID.randomUUID().toString().substring(0, 8);
+        String password = UUID.randomUUID().toString().substring(0, 16);
+
+        // 测试用户服务
+        User user = new User(username, password, "admin");
         assertTrue(userService.insertUser(user) > 0);
-    }
 
-    @Test
-    void testDeleteUserByUserNumber() {
-        String userNumber = "dudu233";
-        assertTrue(userService.deleteUserByUserNumber(userNumber) > 0);
-    }
-
-    @Test
-    void testUpdateUser() {
-        User user = new User("dudu233", "1", "2");
+        user.setPassword(UUID.randomUUID().toString().substring(0, 16));
         assertTrue(userService.updateUser(user) > 0);
-    }
 
-    @Test
-    void testFindUserByUserNumber() {
-        String userNumber = "qzn";
-        assertNotNull(userService.findUserByUserNumber(userNumber));
-    }
-
-    @Test
-    void testFindAllUsers() {
+        assertNotNull(userService.findUserByUserNumber(username));
         assertFalse(userService.findAllUsers().isEmpty());
-    }
 
-    @Test
-    void testInsertTeacher() {
-
+        assertTrue(userService.deleteUserByUserNumber(username) > 0);
     }
 
 
