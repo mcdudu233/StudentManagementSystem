8a9bebabfb6662a4631f39aceaafee928f92c5e0
dudu233 <1147793932@qq.com>
2024-11-19 16:42:01 +0800

    修复没有导包

--- src/main/java/top/mcso/sms/entity/Announcement.java
+++ src/main/java/top/mcso/sms/entity/Announcement.java
@@ -1,12 +1,14 @@
 package top.mcso.sms.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 
 public class Announcement {
-    private String id ;
+    private String id;
     private String announcement;
 }
