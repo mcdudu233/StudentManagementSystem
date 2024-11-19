6d677781f352003008ad6cd0a692b0ade67ab26b
桃源道不清 <xubiaoyu040827@outlook.com>
2024-11-19 13:15:56 +0800

    增加classes类和数据表以及相应的接口层

--- src/main/java/top/mcso/sms/entity/Teacher.java
+++ src/main/java/top/mcso/sms/entity/Teacher.java
@@ -1,12 +1,9 @@
 package top.mcso.sms.entity;
 
-import lombok.*;
+import lombok.Data;
 
-@Setter
-@Getter
+
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class Teacher {
     private String jobNumber;
     private String teacherName;
