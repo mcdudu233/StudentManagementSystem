9961a030a57381c174cb5b875de81969a60e7b2f
桃源道不清 <xubiaoyu040827@outlook.com>
2024-11-20 09:49:19 +0800

    更新Classes类

--- src/main/java/top/mcso/sms/entity/Classes.java
+++ src/main/java/top/mcso/sms/entity/Classes.java
@@ -1,8 +1,12 @@
 package top.mcso.sms.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Classes {
     private int number;
     private String teacherNumber;
