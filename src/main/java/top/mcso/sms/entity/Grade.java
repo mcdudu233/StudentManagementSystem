cb89dea150405f0496f23a5c69cf3ee60d706baf
dudu233 <1147793932@qq.com>
2024-11-21 17:12:18 +0800

    删除不必要的代码

--- src/main/java/top/mcso/sms/entity/Grade.java
+++ src/main/java/top/mcso/sms/entity/Grade.java
@@ -1,12 +1,14 @@
 package top.mcso.sms.entity;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Grade {
-    private String courseNumber ;
+    private String courseNumber;
     private String studentNumber;
     private float grade;
 }
