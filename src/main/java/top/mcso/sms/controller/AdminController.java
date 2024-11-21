5795825c2306861dd0449df9191ee23bd4401d31
dudu233 <1147793932@qq.com>
2024-11-21 10:54:25 +0800

    完善管理员页面

--- src/main/java/top/mcso/sms/controller/AdminController.java
+++ src/main/java/top/mcso/sms/controller/AdminController.java
@@ -1,10 +1,21 @@
 package top.mcso.sms.controller;
 
 
+import jakarta.annotation.Resource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import top.mcso.sms.entity.Teacher;
+import top.mcso.sms.entity.User;
+import top.mcso.sms.service.StudentService;
+import top.mcso.sms.service.TeacherService;
+import top.mcso.sms.service.UserService;
 import top.mcso.sms.utils.SessionUtils;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * 处理管理员页面
@@ -17,6 +28,13 @@ import top.mcso.sms.utils.SessionUtils;
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
+    @Resource
+    private UserService userService;
+    @Resource
+    private StudentService studentService;
+    @Resource
+    private TeacherService teacherService;
+
     @RequestMapping("user")
     public String user(Model model) {
         if (!SessionUtils.isAdmin()) {
@@ -26,6 +44,18 @@ public class AdminController {
         // 设置用户名
         String user = SessionUtils.getName();
         model.addAttribute("user", user);
+
+        // 获得所有用户
+        List<Map<String, String>> users = new ArrayList<>();
+        int id = 0;
+        for (User u : userService.findAllUsers()) {
+            Map<String, String> userMap = new HashMap<>();
+            userMap.put("id", String.valueOf(++id));
+            userMap.put("username", u.getUserNumber());
+            userMap.put("role", u.getPriority());
+            users.add(userMap);
+        }
+        model.addAttribute("users", users);
 
         return "admin/user";
     }
@@ -52,6 +82,18 @@ public class AdminController {
         // 设置用户名
         String user = SessionUtils.getName();
         model.addAttribute("user", user);
+
+        // 获得所有老师
+        List<Map<String, String>> teachers = new ArrayList<>();
+        for (Teacher t : teacherService.findAll()) {
+            Map<String, String> teacherMap = new HashMap<>();
+            teacherMap.put("id", t.getJobNumber());
+            teacherMap.put("name", t.getTeacherName());
+            teacherMap.put("subject", t.getDuty());
+            teacherMap.put("contact", String.valueOf(t.getTelephone()));
+            teachers.add(teacherMap);
+        }
+        model.addAttribute("teachers", teachers);
 
         return "admin/teacher";
     }
